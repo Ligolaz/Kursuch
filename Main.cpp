8ac84685ff22a600e81c05c451fbbea686c9b7ac
Ligolaz <demon0ne0lemon@gmail.com>
2025-05-13 21:07:07 +0300

    ЦИИИИИИИИИИИИИИКЛ
    Still fixing the idea💀💀💀

--- Main.cpp
+++ Main.cpp
@@ -4,23 +4,26 @@
 using namespace std;
 
 int main() {
-	int choice = UserInterface::showMenu(); //Виклик меню користувача
-	PuzzleField field(7, 7); //Маємо поле 7на7 для заповнення
-	if (choice == 1) {
-		field = UserInterface::getFieldFromUser(); //Користувацький ввід
-	} else if (choice == 2) {
-		field = UserInterface::getPresetField(); //Програмний
-	} else {
-		cout << "Exiting program...\n";
-		return 0; //Вихід якщо '3'
-	}
+	int choice; //Змінна для вибору користувача у меню
 
-	Solver solver(field); //Створюємо об'єкт Solver з отриманим полем
-	if (solver.solve()) {
-		cout << "Tetromino results:\n";
-		field.print(); //Вивід результату
-	} else {
-		cout << "Results don't satisfy expectations.\n";
-	}
+	do {
+		choice = UserInterface::showMenu(); //Виклик меню користувача
+		PuzzleField field(7, 7); //Маємо поле 7на7 для заповнення
+		if (choice == 1) {
+			field = UserInterface::getUserField(); //Користувацький ввід
+		} else if (choice == 2) {
+			field = UserInterface::getProgramField(); //Програмний
+		} else if (choice == 3) {
+			cout << "Exiting program...\n";
+			break;
+		}
+		Solver solver(field); //Створюємо об'єкт Solver з отриманим полем
+		if (solver.solve()) {
+			cout << "Tetromino results:\n";
+			field.print(); //Вивід результату
+		} else {
+			cout << "Results don't satisfy expectations.\n";
+		}
+	} while (choice != 3); //Цикл повторюєтся поки не буде '3' як choice
 	return 0;
 }
